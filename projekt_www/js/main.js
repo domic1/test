7150d87d8da89bc50675ce6299779df97cd58538
Dominik <dominik.bacal1@gmail.com>
2023-08-07 11:15:22 +0200

    add footer and conact section

--- projekt_www/js/main.js
+++ projekt_www/js/main.js
@@ -1,9 +1,14 @@
 const nav = document.querySelector('.nav')
 const navBtn = document.querySelector('.burger-btn')
 const allNavItems = document.querySelectorAll('.nav__item')
+const navBtnBars = document.querySelector('.burger-btn__bars')
+const allSections = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year')
 
 const handleNav = () => {
 	nav.classList.toggle('nav--active')
+
+	navBtnBars.classList.remove('black-bars-color')
 
 	allNavItems.forEach(item => {
 		item.addEventListener('click', () => {
@@ -23,3 +28,22 @@ const handleNavItemsAnimation = () => {
 		delayTime++
 	})
 }
+
+const handleObserver = () => {
+	const currenrSection = window.scrollY
+
+	allSections.forEach(section => {
+		if (section.classList.contains('white-section') && section.offsetTop <= currenrSection + 60) {
+			navBtnBars.classList.add('black-bars-color')
+		} else if (!section.classList.contains('white-section') && section.offsetTop <= currenrSection + 60) {
+			navBtnBars.classList.remove('black-bars-color')
+		}
+	})
+}
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+handleCurrentYear()
+navBtn.addEventListener('click', handleNav)
+window.addEventListener('scroll', handleObserver)
