c441364b99a1af89a8d63d7cf0c90737a9dd0999
Dominik <dominik.bacal1@gmail.com>
2023-08-31 10:22:41 +0200

    second way

--- kurs.js/js/index.js
+++ kurs.js/js/index.js
@@ -134,13 +134,15 @@ const num4 = 4
 const number = [1, 2, 3, 4, 5, 6]
 // const  arr =[1, 2, 3, 4, 5, 6, 'sasf", ['asdfasf']]
 
-const btn1 = document.querySelector('button:nth-of-type(1)')
-const btn2 = document.querySelector('button:nth-of-type(2)')
-const btn3 = document.querySelector('button:nth-of-type(3)')
+// const btn1 = document.querySelector('button:nth-of-type(1)')
+// const btn2 = document.querySelector('button:nth-of-type(2)')
+// const btn3 = document.querySelector('button:nth-of-type(3)')
 
-console.log(btn1, btn2, btn3)
+// console.log(btn1, btn2, btn3)
+const btns = document.querySelectorAll('button')
 
 function btnMsg(e) {
 	console.log(`Kliknięto ${e.target.textContent} ! 👻`)
 }
-btn1.addEventListener('click', btnMsg)
+// btn1.addEventListener('click', btnMsg)
+btns.forEach(btn => btn.addEventListener('click', btnMsg))
