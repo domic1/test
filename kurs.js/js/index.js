8ea3b8900bc439453b26a3ba0d2fc5ef5618fdac
Dominik <dominik.bacal1@gmail.com>
2023-08-31 13:03:17 +0200

    map, concat, spread & rest operator

--- kurs.js/js/index.js
+++ kurs.js/js/index.js
@@ -146,14 +146,38 @@ console.log(x);
 // }
 // btn1.addEventListener('click', btnMsg)
 // btns.forEach(btn => btn.addEventListener('click', btnMsg))
-const numbers = [1, 2, 3, 4, 5]
-numbers.unshift(100, 200, 'avta')
-numbers.shift()
-console.log(numbers)
-const colors = ['red', 'green', 'blue']
-console.log(colors)
-colors.push('gold', 123)
-colors.log(colors)
+// const numbers = [1, 2, 3, 4, 5]
+// numbers.unshift(100, 200, 'avta')
+// numbers.shift()
+// console.log(numbers)
+// const colors = ['red', 'green', 'blue']
+// console.log(colors)
+// colors.push('gold', 123)
+// console.log(colors)
 
-colors.pop()
-console.log(colors)
+// colors.pop()
+// console.log(colors)
+// const numbers = [1, 2, 3, 4, 5]
+// function multiply(x) {
+// 	return x * 2
+// }
+// const newNumbers = numbers.map(multiply)
+// console.log(numbers)
+// console.log(newNumbers)
+const numbers = [1, 2, 3, 4, 5]
+
+const abc = ['a', 'b', 'c']
+const newAbc = numbers.concat(6, 7, 8, true, abc)
+
+// console.log(newAbc)
+// console.log(numbers)
+
+console.log(abc)
+console.log(...abc)
+
+const drinks = ['pepsi', 'kawa', 'zupa', 'sok', 'woda']
+const meals = ['schabowy', 'spaghetti', 'deser', 'krokiety']
+
+const menu = [...drinks, ...meals]
+const menu2 = drinks.concat(meals)
+console.log(menu2)
